f49c11a295a60603770a7a74cbcbdb7239446a7d
tomit4 <mosssap@gmail.com>
2024-12-26 16:20:49 -0800

    :construction: Fin chapter 27 exercises and projects

---
+++ chapter_27/programming_projects/04.c
@@ -0,0 +1,32 @@
+#include <complex.h>
+#include <math.h>
+#include <stdio.h>
+
+/* NOTE: Requires -lm flag when compiling */
+
+#define PI (acos(-1.0))
+
+int main(void)
+{
+	int k, n;
+	double complex r;
+
+	printf("Enter positive integer n: ");
+	scanf("%d", &n);
+
+	for (k = 0; k < n; k++) {
+		(r = cexp((2 * PI * I * k) / n));
+
+		if (cimag(r) == 0) {
+			printf("root of unity %d: %g\n", k, creal(r));
+		} else if (cimag(r) == 1 || cimag(r) == -1) {
+			printf("root of unity %d: %g %c i\n", k, creal(r),
+			       cimag(r) < 0 ? '-' : '+');
+		} else {
+			printf("root of unity %d: %g %c %gi\n", k, creal(r),
+			       cimag(r) < 0 ? '-' : '+', cabs((r)));
+		}
+	}
+
+	return 0;
+}
